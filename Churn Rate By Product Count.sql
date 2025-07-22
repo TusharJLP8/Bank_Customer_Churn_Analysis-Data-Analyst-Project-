4d07227aab63baaeb25872040f8ffc700de9214b
Tushar JL Palve <palvetushar0@gmail.com>
2025-07-22 22:52:32 +0530

    Delete Churn Rate By Product Count.sql

--- Churn Rate By Product Count.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-  NumOfProducts,
-  COUNT(*) AS Total_Customers,
-  SUM(Exited) AS Churned,
-  ROUND(SUM(Exited) * 100.0 / COUNT(*), 2) AS Churn_Rate
-FROM customer_merged
-GROUP BY NumOfProducts
-ORDER BY NumOfProducts;
